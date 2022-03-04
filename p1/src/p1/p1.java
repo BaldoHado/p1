77f91558c8e64ca4b6b8800301f10447fcfa6216
Ta Man 04 <adamta2004@gmail.com>
2022-03-04 09:22:42 -0800

    Coordinate map input done. Working on algorithm

--- p1/src/p1/p1.java
+++ p1/src/p1/p1.java
@@ -1,7 +1,9 @@
 package p1;
 
 import java.io.File;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -9,6 +11,7 @@ public class p1 {
 	
 	private static char[][] lines;
 	private static ArrayList<Coord> coordinates = new ArrayList<Coord>();
+	private static ArrayList<Coord> coords = new ArrayList<Coord>();
 	
 	public static void main(String[] args) {
 		
@@ -56,7 +59,7 @@ public class p1 {
 		
 		for (int i = 0; i < coordinates.size(); i++) {
 			if (coordinates.get(i).getVal() == "K".charAt(0)) {
-				findCakeStacks(coordinates.get(i).getRow(),coordinates.get(i).getCol());
+				//findCakeStacks(coordinates.get(i).getRow(),coordinates.get(i).getCol());
 				System.out.println("Kirby at " +  coordinates.get(i).getRow() + " " +coordinates.get(i).getCol());
 				
 			}
@@ -64,31 +67,55 @@ public class p1 {
 		}
 	}
 	
-	public static void findCakeStacks(int row, int col) {
+	public static void coordinateBasedArr(Scanner scan)  {
+		scan.nextLine();
+		//take in cols and # of rows
+		while(scan.hasNextLine()) {
+			String line = scan.nextLine();
 	
-		boolean foundCake = false;
-		Stack temp = new Stack();
-		Stack vals = new Stack();
-		int curRow = row;
-		int curCol = col;
-/*		for (int i =0;i<coordinates.size();i++) {
-			System.out.println("" + coordinates.get(i).getVal() + " " + coordinates.get(i).getRow() + " " +coordinates.get(i).getCol());
-		}*/	
-	
-		if (lines[curRow+1][curCol] == ".".charAt(0)) {
-	
-		}
-		if (lines[curRow][curCol+1] == ".".charAt(0)) {
-	
-		}
-		if (lines[curRow-1][curCol] == ".".charAt(0)) {
-				
-		}
-		if (lines[curRow][curCol-1] == ".".charAt(0)) {
+		
+			Coord t = new Coord(Integer.valueOf(String.valueOf(line.charAt(1))),Integer.valueOf(String.valueOf(line.charAt(2))),line.charAt(0));
+			//System.out.println(t.getRow());
+			coords.add(t);
 			
 		}
+			
+		for (int i = 0; i < coords.size(); i++) {
+			if (coords.get(i).getVal() == "K".charAt(0)) {
+				//findCakeStacks(coordinates.get(i).getRow(),coordinates.get(i).getCol());
+				System.out.println("Kirby at " +  coords.get(i).getRow() + " " +coords.get(i).getCol());
+				
+			}
+			
+		}
+	}
+	public static void findCakeStacks(int row, int col) {
+	
+		ArrayDeque<Coord> temp = new ArrayDeque<Coord>();
+		ArrayDeque<Coord> vals = new ArrayDeque<Coord>();
+		int curRow = row;
+		int curCol = col;
+		for (int i =0;i<coordinates.size();i++) {
+			System.out.println("" + coordinates.get(i).getVal() + " " + coordinates.get(i).getRow() + " " +coordinates.get(i).getCol());
+		}
+		if (curRow > 0 && curCol > 0) {
+			if (lines[curRow+1][curCol] == ".".charAt(0)) {
+				temp.add(new Coord(curRow,curCol,lines[curRow][curCol]));
+			}
+			if (lines[curRow][curCol+1] == ".".charAt(0)) {
+				temp.add(new Coord(curRow,curCol+1,lines[curRow][curCol+1]));
+			}
+			if (lines[curRow-1][curCol] == ".".charAt(0)) {
+				temp.add(new Coord(curRow-1,curCol,lines[curRow-1][curCol]));
+			}
+			if (lines[curRow][curCol-1] == ".".charAt(0)) {
+				temp.add(new Coord(curRow,curCol-1,lines[curRow][curCol-1]));
+			}
+		}
+		
 		
 	}
-	
 
 }
+
+
