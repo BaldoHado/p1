d4e981764749114cc895262231265375a001fbfa
adamt <adamt@10.9.136.159>
2022-03-17 09:24:20 -0700

    Finished the optimal path

--- p1/src/p1/p1.java
+++ p1/src/p1/p1.java
@@ -14,11 +14,16 @@ public class p1 {
 	private static ArrayList<Coord> coords = new ArrayList<Coord>();
 	private static ArrayDeque<Coord> temp;
 	private static ArrayDeque<Coord> vals;
+	private static ArrayDeque<Coord> path;
+	private static ArrayList<Coord> doors;
+	private static ArrayList<Coord> kirbyLoc; 
 	private static Stack<Coord> tempSt;
 	private static Stack<Coord> valsSt;
 	private static int rows;
 	private static int cols;
 	private static int rooms;
+	private static int tRooms;
+	private static int tDoor;
 	private static Coord Cake;
 	private static int amtDoors;
 	
@@ -28,6 +33,10 @@ public class p1 {
 		File f = new File("adamMap.txt");
 		temp = new ArrayDeque<Coord>();
 		vals = new ArrayDeque<Coord>();
+		path = new ArrayDeque<Coord>();
+		doors = new ArrayList<Coord>();
+		kirbyLoc = new ArrayList<Coord>();
+		tDoor = 0;
 
 		try {
 			scanner = new Scanner(f);
@@ -74,6 +83,7 @@ public class p1 {
 				Coord c = new Coord(coordinates.get(i).getRow(),coordinates.get(i).getCol(),coordinates.get(i).getVal());
 				
 				findCakeQueue(c);
+				makePath(c,vals.remove());
 				System.out.println(Cake.getRow() + " " + Cake.getCol());
 				break;
 				//System.out.println("Kirby at " +  coordinates.get(i).getRow() + " " +coordinates.get(i).getCol());
@@ -102,13 +112,63 @@ public class p1 {
 				Coord c = new Coord(coords.get(i).getRow(),coords.get(i).getCol(),coords.get(i).getVal());
 				findCakeQueue(c);
 				System.out.println(Cake.getRow());
+				makePath(c,vals.remove());
 				System.out.println("Kirby at " +  coords.get(i).getRow() + " " +coords.get(i).getCol());
 				System.out.println("T SIZE = " + temp.size());
 			}
 		}
 	}
 	
-	public static void makePath() { 
+	public static void makePath(Coord curr, Coord c) { //if multiple rooms, look for door first, then last floor look for cake
+		System.out.println("tRooms: " + tRooms + " rooms: " + rooms);
+		if (tRooms == rooms-1) {
+			if (vals.peek().getVal() == "C".charAt(0)) {
+				System.out.println("FOUND cAKE BRO!" );
+			} else if ((Math.abs(c.getRow()-Cake.getRow()) < Math.abs(curr.getRow()-Cake.getRow())) ||(Math.abs(c.getCol()-Cake.getCol()) < Math.abs(curr.getCol()-Cake.getCol()))) {
+				path.add(c);
+				System.out.println(" WALKING TO KIRBS " + c.getRow() + " " + c.getCol());
+				if (vals.size() >= 1) {
+					//System.out.println(vals.remove().getRow());
+					makePath(c,vals.remove());
+					//System.out.println("Vals: " + vals.size());
+				}
+				
+			} else {
+				if (vals.size() >= 1) {
+					System.out.println("Fail");
+					//System.out.println("Vals: " + vals.size());
+					makePath(curr,vals.remove());
+				}
+			}
+		} else {
+			//System.out.println(" DOOR PEEK " + doors.get(tRooms).getRow() + " " + doors.get(tRooms).getCol() + " " + doors.get(tRooms).getVal());
+			//System.out.println(" VAL PEEK " + vals.peek().getRow() + " " + vals.peek().getCol());
+			//System.out.println(Integer.valueOf(vals.peek().getRow()) == Integer.valueOf(doors.get(tRooms).getRow()) && Integer.valueOf(vals.peek().getRow()) == Integer.valueOf(doors.get(tRooms).getCol()));
+			if (vals.peek().getVal() == "|".charAt(0)) {				
+				System.out.println("YO DOOR" + " " + vals.peek().getRow() + " " + vals.peek().getCol() +" "+  vals.peek().getVal());
+				System.out.println("Starting kirby at " + kirbyLoc.get(tRooms).getRow() + " " + kirbyLoc.get(tRooms).getCol());
+				System.out.println(vals.size() + "Kib size " + kirbyLoc.size());
+				vals.remove();
+				tRooms++;
+				makePath(kirbyLoc.get(tRooms-1),vals.remove());
+			}else if ((Math.abs(c.getRow()-doors.get(tRooms).getRow()) < Math.abs(curr.getRow()-doors.get(tRooms).getRow())) || (Math.abs(c.getCol()-doors.get(tRooms).getCol()) < Math.abs(curr.getCol()-doors.get(tRooms).getCol()))  ) {
+					path.add(c);
+					System.out.println("DOOR:" + c.getRow() + " " + c.getCol());
+					if (vals.size() >= 1) {
+						//System.out.println(vals.remove().getRow());
+						System.out.println("MAKING PATH");
+						System.out.println(vals.peek().getRow() + " " + vals.peek().getCol() +" "+  vals.peek().getVal());
+						makePath(c,vals.remove());	
+						//System.out.println("Vals: " + vals.size());
+					}
+			}else {
+				if (vals.size() >= 1) {
+					System.out.println("Fail");
+					//System.out.println("Vals: " + vals.size());
+					makePath(curr,vals.remove());
+				}
+			}
+		}
 		
 	}
 	
@@ -117,7 +177,7 @@ public class p1 {
 		int curRow = c.getRow();
 		int curCol = c.getCol();
 		System.out.println(c.getRow() + " " + c.getCol());
-	
+		
 		if (lines[curRow][curCol] == "C".charAt(0)) {
 			System.out.println("C at " + c.getRow() + " " + c.getCol());
 			Cake = c;
@@ -130,14 +190,18 @@ public class p1 {
 
 		}
 		if(lines[curRow][curCol] == "|".charAt(0)) {
-			vals.add(c);
-			System.out.println("Door detected");
+			System.out.println(vals.size() + " SiZE");
+			vals.add(new Coord(curRow,curCol,lines[curRow][curCol]));
+			doors.add(new Coord(curRow,curCol,lines[curRow][curCol]));
+			System.out.println("Door detected" + " DOOR SIZE: " + doors.size() + " door row" + c.getRow() + " " + c.getCol());
+			
 			int startRow = 0;
 			int startCol = 0;
 			amtDoors++;
 			for (int i = amtDoors*rows-1; i < (amtDoors+1)*rows-1; i++) {
 				for (int j = 0; j < cols;j++) {
 					if (lines[i][j] == "K".charAt(0)) {
+						kirbyLoc.add(new Coord(i,j,lines[i][j]));
 						System.out.println("I " + i + " J " + j);
 						startRow = i;
 						startCol = j;
@@ -253,7 +317,7 @@ public class p1 {
 		int tRow = c.getRow();
 		int tCol = c.getCol();
 		ArrayDeque<Coord> tList = new ArrayDeque<Coord>();
-		Coord low;
+		Coord low = null;
 		boolean found = false;
 		
 		while (vals.size() > 0 && found == false) {
@@ -266,7 +330,12 @@ public class p1 {
 			}
 		}
 		
+		while (tList.size() > 0) {
+			Coord t = tList.remove();
+			vals.add(t);
+		}
 		
+		return low;
 		
 	}
 	
